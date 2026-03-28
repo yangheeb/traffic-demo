e404c3f8e7f15c9b868cea02dd165e7390cd118e
백민정 <ggamji09@naver.com>
2026-03-28 10:56:41 +0900

    first commit

---
+++ src/main/java/dev/fisa/TrafficDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fisa/TrafficDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
